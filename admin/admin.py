1ff0be3ab68ea30a34f6c157a29e4da72f1a24e3
mhe1999 <ehteshami1378@gmail.com>
2020-07-25 12:24:58 +0430

    add 'Customers INFO' page

---
+++ admin/admin.py
@@ -0,0 +1,72 @@
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
+from pymongo import MongoClient
+from utils.datatable import  DataTable
+import pyodbc
+
+
+class AdminWindow(BoxLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        # self.widgettable = Label()
+        # self.outtt()
+
+    def on_spinner_select(self, text):
+        print (text)
+        table_name = ''
+        if text == 'Customer table':
+            table_name = 'Customer'
+        elif text == 'transactions':
+            table_name = 'Trn_Src_Des'
+        elif text == 'Deposits':
+            table_name = 'Deposit'
+        elif text == 'Deposits type':
+            table_name = 'Deposit_Type'
+        elif text == 'Deposits status':
+            table_name = 'Deposit_Status'
+        elif text == 'Branchs':
+            table_name = 'Branch'
+
+        self.outtt(table_name)
+
+    def outtt(self , table_name):
+        conn = pyodbc.connect('Driver={SQL Server};'
+                                  'Server=DESKTOP-7MGG4HH;'
+                                  'Database=bank_trn;'
+                                  'Trusted_Connection=yes;')
+
+        cursor = conn.cursor()
+        cursor.execute('SELECT * FROM bank_trn.dbo.%s' % (table_name))
+        SQLtable = cursor.fetchall()
+
+        cursor.execute("SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS where TABLE_NAME = '%s'" % (table_name,))
+        SQLheaders = cursor.fetchall()
+
+        self.widgettable = DataTable(table = SQLtable, SQLheaders = SQLheaders)
+        content = self.ids.Bscrn_content
+        content.clear_widgets()
+        whatever = self.ids.whatever
+
+        content.add_widget(whatever)
+        content.add_widget(self.widgettable)
+
+
+
+
+    def change_screen(self, instance):
+        if instance.text == 'Manage Products':
+            self.ids.scrn_mngr.current = 'scrn_product_content'
+        elif instance.text =='Manage Users':
+            self.ids.scrn_mngr.current = 'scrn_content'
+        else:
+            self.ids.scrn_mngr.current = 'scrn_analysis'
+
+
+
+class AdminApp(App):
+    def build(self):
+        return AdminWindow()
+
+if __name__ == '__main__':
+    AdminApp().run()
---
+++ admin/utils/datatable.py
@@ -0,0 +1,64 @@
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.lang import Builder
+
+from pymongo import MongoClient
+Builder.load_string('''
+<DataTable>:
+    id:main_win
+    RecycleView:
+        viewclass: 'custLabel'
+        id: table_floor
+        RecycleGridLayout:
+            id: table_floor_layout
+            cols: 5
+            default_size: (None,250)
+            default_size_hint: (1,None)
+            size_hint_y: None
+            height: self.minimum_height
+            spacing: 5
+
+<custLabel@Label>:
+    bcolor: (1,1,1,1)
+    canvas.before:
+        Color:
+            rgba: root.bcolor
+        Rectangle:
+            pos: self.pos
+            size: self.size
+''')
+class DataTable(BoxLayout):
+    def __init__(self, table='', SQLheaders='', **kwargs):
+    # widgettable = DataTable(table = SQLtable, SQLheaders = SQLheaders)
+        super().__init__(**kwargs)
+        # products = self.get_products()
+        products = table
+        col_titles = []
+        for k in SQLheaders:
+            col_titles.append(k[0])
+        rows_len = len(products[0])
+        total_rows = len(products)
+        self.columns = len(col_titles)
+        print(rows_len)
+        print(total_rows)
+        table_data = []
+        for t in col_titles:
+            table_data.append({'text':str(t), 'size_hint_y':None, 'height':50, 'bcolor':(.06,.45,.45,1)})
+            # table_data.append({'text':str(t)})
+        for t in range(total_rows//2):
+            for r in range(rows_len):
+                table_data.append({'text':str(products[2*t][r]), 'size_hint_y':None, 'height':30, 'bcolor':(.06,.25,.25,1)})
+            for r in range(rows_len):
+                table_data.append({'text':str(products[2*t +1][r]), 'size_hint_y':None, 'height':30, 'bcolor':(.06,.55,.55,1)})
+                # table_data.append({'text':str(products[t][r])})
+
+        self.ids.table_floor_layout.cols = self.columns
+        self.ids.table_floor.data = table_data
+
+
+# class DataTableApp(App):
+#     def build(self):
+#         return DataTable()
+#
+# if __name__ == '__main__':
+#     DataTableApp().run()
