93f885950a15c1045ae712b92328ef6832027748
mhe1999 <ehteshami1378@gmail.com>
2020-07-25 23:32:39 +0430

    add query executaion to 'CRUD page'

--- admin/admin.py
+++ admin/admin.py
@@ -57,12 +57,14 @@ class AdminWindow(BoxLayout):
             self.ids.scrn_mngr.current = 'scrn_graph'
         elif instance.text =='customers INFO':
             self.ids.scrn_mngr.current = 'scrn_customerINFO'
+            if self.ids.whatever.text != 'Choose a table':
+                self.on_spinner_select(self.ids.whatever.text)
         else:
             self.ids.scrn_mngr.current = 'scrn_CRUD'
             # self.ids.voucherID.focus = 'True'
 
     def insert_data(self):
-        if str(self.ids.voucherID.text) == '':
+        if self.ids.voucherID.text == '':
             self.ids.error_message.text = '[color=#FF0000]voucherID cant be empty[/color]'
         elif self.ids.voucherID.text.isnumeric() == False :
             self.ids.error_message.text = '[color=#FF0000]voucherID should be a number[/color]'
@@ -82,8 +84,64 @@ class AdminWindow(BoxLayout):
             self.ids.error_message.text = '[color=#FF0000]Source_Deposit should be a number[/color]'
         elif self.ids.Destination_Deposit.text.isnumeric() == False and self.ids.Destination_Deposit.text != '' :
             self.ids.error_message.text = '[color=#FF0000]Destination_Deposit should be a number[/color]'
+        else:
+            self.ids.error_message.text = '[color=#00FF00]every things good, working on it...[/color]'
+
+            sql_voucherID = self.ids.voucherID.text
+            sql_date = self.ids.date.text
+            sql_time = self.ids.time.text
+            sql_amount = int(self.ids.amount.text)
+            if self.ids.Source_Deposit.text != '':
+                sql_Source_Deposit = int(self.ids.Source_Deposit.text)
+            else:
+                sql_Source_Deposit = None
+            if self.ids.Destination_Deposit.text != '':
+                sql_Destination_Deposit = int(self.ids.Destination_Deposit.text)
+            else:
+                sql_Destination_Deposit = None
+            if self.ids.Branch_ID.text != '':
+                sql_Branch_ID = int(self.ids.Branch_ID.text)
+            else:
+                sql_Branch_ID = None
+
+            try:
+                conn = pyodbc.connect('Driver={SQL Server};'
+                                          'Server=DESKTOP-7MGG4HH;'
+                                          'Database=bank_trn;'
+                                          'Trusted_Connection=yes;')
+
+                cursor = conn.cursor()
+                cursor.execute('INSERT INTO bank_trn.dbo.Trn_Src_Des VALUES (?, ?, ?, ?, ?, ?, ?, ?)' , (sql_voucherID, sql_date, sql_time, sql_amount, sql_Source_Deposit, sql_Destination_Deposit, sql_Branch_ID, None))
+                conn.commit()
+                # cursor.execute('INSERT INTO bank_trn.dbo.Trn_Src_Des VALUES (%s, %s, %s, %s, %s, %s, %s, %s)' % ('5555', '1400-01-01', '010101', 1000, None, 14, None, None))
+                self.ids.error_message.text = '[color=#00FF00]row successfuly added[/color]'
+
+            except:
+                self.ids.error_message.text = '[color=#FF0000]there is a problem with server[/color]'
 
 
+    def delete_data(self):
+        if self.ids.voucherID_delete.text == '':
+            self.ids.error_message_delete.text = '[color=#FF0000]voucherID cant be empty[/color]'
+        elif self.ids.voucherID_delete.text.isnumeric() == False :
+            self.ids.error_message_delete.text = '[color=#FF0000]voucherID should be a number[/color]'
+
+        else:
+            sql_voucherID = self.ids.voucherID_delete.text
+            try:
+                conn = pyodbc.connect('Driver={SQL Server};'
+                                          'Server=DESKTOP-7MGG4HH;'
+                                          'Database=bank_trn;'
+                                          'Trusted_Connection=yes;')
+
+                cursor = conn.cursor()
+                cursor.execute('DELETE FROM bank_trn.dbo.Trn_Src_Des WHERE VoucherId = %s' % (sql_voucherID,))
+                conn.commit()
+                # cursor.execute('INSERT INTO bank_trn.dbo.Trn_Src_Des VALUES (%s, %s, %s, %s, %s, %s, %s, %s)' % ('5555', '1400-01-01', '010101', 1000, None, 14, None, None))
+                self.ids.error_message_delete.text = '[color=#00FF00]row successfuly deleted[/color]'
+
+            except:
+                self.ids.error_message_delete.text = '[color=#FF0000]there is a problem with server[/color]'
 
 
 class AdminApp(App):
